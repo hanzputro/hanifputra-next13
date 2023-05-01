82485a8a25acb7a8eec52e41c7567866fb3f97de
hanif.ptr <hanif.putra@qeon.co.id>
2023-05-02 00:04:11 +0700

    create json data for project

--- src/components/Section/Project.tsx
+++ src/components/Section/Project.tsx
@@ -1,27 +1,56 @@
 import React from "react";
 import Image from "next/image";
-import { Andada_Pro, Inter } from "next/font/google";
+import { Andada_Pro } from "next/font/google";
+import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
 
 const andadaPro = Andada_Pro({
   weight: ["600"],
   subsets: ["latin"],
 });
 
-const inter = Inter({
-  weight: ["400"],
-  subsets: ["latin"],
-});
+export interface ProjectDetailType {
+  title: String;
+  tags: [];
+  description: String;
+  image: String;
+  thumbnail: String;
+}
 
-const Project = () => {
+interface ProjectProps {
+  project: ProjectDetailType[];
+}
+
+const Project = ({ project }: ProjectProps) => {
   return (
-    <section>
-      <div>
-        <h2 className={`${andadaPro.className} text-[86px]`}>PROJECT</h2>
-        <h2 className={`${andadaPro.className} text-[50px]`}>
-          WORKS<span className="inline-block text-[#FFEE00]">_</span>
+    <section className="relative w-full px-24 pt-24">
+      <div className="flex items-center mb-10">
+        <h2
+          className={`${andadaPro.className} text-[170px] text-[#111111] leading-[0.75] tracking-[-10px] opacity-[0.018] blur-[2px] ml-[-68px]`}
+        >
+          PROJECT
+        </h2>
+        <h2 className={`${andadaPro.className} text-[50px] absolute`}>
+          Works<span className="inline-block text-[#FFEE00]">_</span>
         </h2>
       </div>
-      <div></div>
+      <div>
+        <ResponsiveMasonry columnsCountBreakPoints={{ 350: 2, 750: 3, 900: 4 }}>
+          <Masonry>
+            {project?.map((item) => {
+              return (
+                <Image
+                  className="relative"
+                  src={`/assets/images/project/thumb/${item.image}`}
+                  alt="anjar tanjung"
+                  width={400}
+                  height={200}
+                  priority
+                />
+              );
+            })}
+          </Masonry>
+        </ResponsiveMasonry>
+      </div>
     </section>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,34 @@
 import Header from "@/components/Header";
 import Hero from "@/components/Section/Hero";
+import Project, { ProjectDetailType } from "@/components/Section/Project";
+import fsPromises from "fs/promises";
+import path from "path";
 
-export default function Home() {
+interface HomeProps {
+  project: ProjectDetailType[];
+}
+
+export default function Home(props: HomeProps) {
   return (
     <>
       <Header />
       <main className="flex min-h-screen flex-col items-center justify-between">
         <Hero />
+        <Project project={props.project} />
       </main>
     </>
   );
+}
+
+// Fetching data from the JSON file
+export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), "src/data/project.json");
+  const jsonData: HomeProps["project"] | any = await fsPromises.readFile(
+    filePath
+  );
+  const objectData = JSON.parse(jsonData);
+
+  return {
+    props: objectData,
+  };
 }
