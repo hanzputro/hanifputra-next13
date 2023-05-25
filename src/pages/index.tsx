f166872252b4d20461632b4cee9eb6ac0d17d714
hanzputro <hanzputro@gmail.com>
2023-05-25 23:08:21 +0700

    add skill section & skill logo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,12 +2,14 @@ import Header from "@/components/Header";
 import Hero from "@/components/Section/Hero";
 import Project, { ProjectDetailType } from "@/components/Section/Project";
 import Contact, { SocialMediaDetailType } from "@/components/Section/Contact";
+import Skill, { SkillType } from "@/components/Section/Skill";
 import fsPromises from "fs/promises";
 import path from "path";
 
 interface HomeProps {
   project: ProjectDetailType[];
   socialMedia: SocialMediaDetailType[];
+  skill: SkillType[];
 }
 
 export default function Home(props: HomeProps) {
@@ -16,6 +18,7 @@ export default function Home(props: HomeProps) {
       <Header />
       <main className="flex min-h-screen flex-col items-center justify-between">
         <Hero />
+        <Skill skill={props.skill} />
         <Project project={props.project} />
         <Contact socialMedia={props.socialMedia} />
       </main>
@@ -36,10 +39,17 @@ export async function getStaticProps() {
     await fsPromises.readFile(fileSocialMedia);
   const socialMediaData = JSON.parse(jsonSocialMediaData);
 
+  const fileSkill = path.join(process.cwd(), "src/data/skill.json");
+  const jsonSkillData: HomeProps["skill"] | any = await fsPromises.readFile(
+    fileSkill
+  );
+  const skillData = JSON.parse(jsonSkillData);
+
   return {
     props: {
       ...projectData,
       ...socialMediaData,
+      ...skillData,
     },
   };
 }
